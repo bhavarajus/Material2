ab4d248dc3d1b5302dc6c6c023643341ccbe1103
bhavarajus <s.bhavaraju@fintra.in>
2018-03-30 12:00:52 +0530

    SB: changes made.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-declare var $: any;
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit() {
-    this.fucntion();
+export class AppComponent {
+  searchInput = false;
+  search: any;
+  searchInputClick = () => {
+    this.searchInput = true;
   }
-  fucntion = () => {
-    $("#searchToggle").click(function() {
-        $("#search").toggle();
-        $("#doNotSearch").toggle();
-    });
+  closeSearchClick = () => {
+    this.searchInput = false;
+    this.search = '';
   }
 }
