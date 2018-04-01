6ede42856f622af1784d7460d70a0b30ab4954e2
bhavarajus <s.bhavarajus@fintra.in>
2018-04-01 10:56:08 +0530

    added comments.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+// Observable: reprents a set of values.
 import { Observable } from 'rxjs/Observable';
+// startwith: Observable emits specific items before emitting all items normally expected from the output.
 import { startWith } from 'rxjs/operators/startWith';
-// map() it passes each source value through a function to get changing output values.
+// map(): it passes each source value through a function to get changing output values.
 import { map } from 'rxjs/operators/map';
 declare var $: any;
 @Component({
@@ -12,9 +14,13 @@ declare var $: any;
 })
 export class AppComponent {
   searchInput = false;
+  // Model object.
   search: any;
+  // FormControl.
   valueCtrl: FormControl;
+  // Expected result stored in this array.
   allValues: Observable<any[]>;
+  // Declaring a JSON for expected results.
   value = [
     { name: 'Ahmed' },
     { name: 'Vamshi' },
@@ -22,16 +28,22 @@ export class AppComponent {
     { name: 'Samuel' }
   ];
   constructor() {
+    // Start of the page with these steps.
+    // Setting a new form control
     this.valueCtrl = new FormControl();
+    // Giving values to this array.
     this.allValues = this.valueCtrl.valueChanges.pipe(startWith(''), map(searchValues => searchValues ? this.filterValues(searchValues) : this.value.slice()));
   }
+  // Filtering values based on name and the 1st option of the typed value.
   filterValues(name) {
     return this.value.filter(searchValues =>
       searchValues.name.indexOf(name) === 0);
   }
+  // Setting searchInput which is normal toolbar page without click of search button.
   searchInputClick = () => {
     this.searchInput = true;
   }
+  // Hiding the remaining buttons and showing search bar.
   closeSearchClick = () => {
     this.searchInput = false;
     this.search = '';
