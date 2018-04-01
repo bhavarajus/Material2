9e4fde6f30a1b64d2ab1aa8e3813ce89d52bebd8
bhavarajus <s.bhavarajus@fintra.in>
2018-04-01 10:31:46 +0530

    removed filter pipe.

--- src/app/toolbar.module.ts
+++ src/app/toolbar.module.ts
@@ -1,7 +1,6 @@
 import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatIconModule, MatAutocompleteModule } from '@angular/material';
-import { TruncatePipe, OrderBy } from './filter-pipe';
 @NgModule({
   imports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatIconModule, MatAutocompleteModule],
   exports: [MatButtonModule, MatToolbarModule, MatInputModule, MatProgressSpinnerModule, MatCardModule, MatIconModule, MatAutocompleteModule],
